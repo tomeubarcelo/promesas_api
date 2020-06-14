f8c6912b80a6294841f6c6dfcf422ac59f289c32
Tomeu Barcelo <tomeu.95@gmail.com>
2020-06-14 10:33:37 +0200

    Added node_modules

---
+++ src/promesas/index.js
@@ -0,0 +1,10 @@
+/*  ********************************
+ * @title Ejercicio promesas api
+ * @version 0.0.1
+ * @author Tomeu Barceló
+ ******************************** */
+
+/*
+Resolver la petición a la API pero con Promesas
+Siguiendo la estructura de NPM y marcando los git, cuando vayamos avanzando en nuestro código que solo se vera con 'npm run'.
+*/
